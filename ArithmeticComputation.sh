856700bd8d4e129330853f979e407cd1cc2c31bd
Saiprasad8341 <saiprasad.8341@gmail.com>
2022-09-27 15:42:28 +0530

    UC5 - Compute a%b+c

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -4,4 +4,4 @@ read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
-echo `awk 'BEGIN{printf("%0.2f", '$c' + '$a' / '$b' )}'`
+echo $(( $a % $b + $c ))
